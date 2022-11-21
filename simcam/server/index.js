6d33033f3ddd8f333d7f1afe07ba5eeef7cb0816
David Cardinal <github@cardinalphoto.com>
2022-11-21 08:50:40 -0800

    add ability to run server code

--- simcam/server/index.js
+++ simcam/server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const apiPort = 3001
+const { spawn, spawnSync } = require('child_process');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -16,7 +17,12 @@ app.get('/', (req, res) => {
 // Here is where we want to try to do something!
 // We can start by doing a simple GET to try a spawn
 app.get('/compute', (req, res) => {
-    res.send('Hello World!')
+    console.log('Got Compute Request\n');
+    const ls = spawnSync('ls', ['-lh', '.']);
+    console.log('Finished Compute Request\n');
+
+    res.send('Found:' + ls);
+
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
