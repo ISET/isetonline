11fc9393acd2e715b56370d24b9454be560274d7
David Cardinal <github@cardinalphoto.com>
2023-01-16 13:13:43 -0800

    Get ground truth from .exr files and store with autoscene

--- scripts/s_storeAutoSceneData.m
+++ scripts/s_storeAutoSceneData.m
@@ -1,11 +1,15 @@
 % Simple script to create DB Documents for the Ford scenes
 % 
-% Currently only saves obvious metadata
-% Ideally would correlate the annotation files and
-% save easier to use versions of targets / GT
+% Currently saves obvious metadata
+% Adding ability to save GTObjects
 
-sceneFolder = fullfile(iaFileDataRoot(), 'Ford', 'sceneMetadata');
+projectName = 'Ford'; % we currently use folders per project
+projectFolder = fullfile(iaFileDataRoot('local', true), projectName); 
+sceneFolder =  fullfile(projectFolder, 'SceneMetadata');
 sceneDataFiles = dir(fullfile(sceneFolder,'*.mat'));
+
+EXRFolder = fullfile(projectFolder, 'SceneEXRs');
+infoFolder = fullfile(projectFolder, 'additionalInfo');
 
 useCollection = 'autoScenes';
 
@@ -23,6 +27,22 @@ for ii = 1:numel(sceneDataFiles)
     sceneMeta.project = "Ford Motor Company";
     sceneMeta.creator = "Zhenyi Liu";
     sceneMeta.sceneSource = "Blender";
+
+    % Update dataset folder to new layout
+    sceneMeta.datasetFolder = EXRFolder;
+
+    % in theory we can get the ground truth from the original
+    % .exr files. Do we need these .mat files?
+    instanceFile = fullfile(EXRFolder, ...
+            sprintf('%s_instanceID.exr', imageID));
+    additionalFile = fullfile(infoFolder, ...
+            sprintf('%s.txt',imageID));
+
+    GTObjects = olGetGroundTruth([], 'instanceFile', instanceFile, ...
+        'additionalFile', additionalFile);
+
+    % Store whatever ground truth we can calculate
+    sceneMeta.GTObject = GTObjects;
 
     % instance and depth maps are too large as currently stored
     sceneMeta.instanceMap = [];
