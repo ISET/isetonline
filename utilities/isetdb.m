600137a5856a5ad3a2889bbbadc5c9540965e926
David Cardinal <github@cardinalphoto.com>
2023-01-20 15:02:15 -0800

    change db to idb to avoid conflicts

---
+++ utilities/isetdb.m
@@ -0,0 +1,10 @@
+function dbObject = isetdb()
+%ISETDB Open default ISET database
+
+try 
+    dbObject = idb.ISETdb();
+catch
+    warning(" Unable to open db, make sure you have prefs 'db', 'server' and 'db' 'port' set.");
+
+end
+
