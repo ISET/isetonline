1fba40359a3c044b5ae15d84d0d597037544b764
David Cardinal <github@cardinalphoto.com>
2023-01-20 09:48:09 -0800

    extend find command to support queries

--- @db/find.m
+++ @db/find.m
@@ -1,5 +1,19 @@
 function documents = find(obj,collection, varargin)
 %FIND Return all the documents that match a find command on a collection
+% 
+% Input:
+%   Our db object
+%   collection name
+%   (optional) query string
+%
+% Output:
+%   matching documents
+%
+% Example:
+%   ourDB.find('autoScenesEXR');
+%
+% D.Cardinal, Stanford University, 2023
+%
 
 % Assume our db is open & query
 if ~isopen(obj.connection)
@@ -7,8 +21,18 @@ if ~isopen(obj.connection)
     return;
 end
 
+p = inputParser();
+addParameter(p, 'query', '', @ischar);
+
+p.Parse(varargin{:});
+query = p.Resuls.query;
+
 try
-    documents = find(obj.connection, collection);
+    if isempty(query)
+        documents = find(obj.connection, collection);
+    else
+        documents = find(ojb.connection, collection, Query = query);
+    end
 catch
     documents = [];
 end
---
+++ @db/getGTfromScene.m
@@ -0,0 +1,41 @@
+function GTObjects = getGTfromScene(obj, sceneType, sceneID)
+%GETGTFROMSCENE retrieve Ground truth from a scene in the database
+% Currently supports auto scenes rendered for the Ford project
+%
+% Input:
+%   Scenetype -- 'auto'
+%   SceneID   -- <ID of the desired scene>
+% 
+% Output:
+%   GTObject structure
+%
+% Example:
+%   ourDB.getGTfromScene('auto', '1112153442');
+%
+% D.Cardinal, Stanford University, 2023
+
+% Assume our db is open & query
+if ~isopen(obj.connection)
+    return;
+end
+
+% We only support auto scenes for now
+switch sceneType
+    case 'auto'
+        dbTable = 'autoScenesEXR';
+        % sceneIDs are unique for auto scenes
+        queryString = sprintf("{""sceneID"": ""%s""}", sceneID);
+        ourScene = obj.find(dbTable, queryString);
+        if exists(ourScene.GTObjects)
+            GTObjects = ourScene.GTObjects;
+        else
+            GTObjects = [];
+        end
+    case other
+        warning("Scene Type %s not supported", sceneType);
+        GTObjects = [];
+end
+
+
+end
+
