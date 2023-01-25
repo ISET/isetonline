8b0eb83e919fde5bcf621e04e5d0ea9947e8a01b
David Cardinal <github@cardinalphoto.com>
2023-01-25 10:50:19 -0800

    classdef

--- @scenario/scenario.m
+++ @scenario/scenario.m
@@ -1,6 +1,23 @@
-function scenario = scenarioCreate(varargin)
-%SCENARIOCREATE Create a scenario struct that can be run
-%   Detailed explanation goes here
+classdef scenario < handle
+    %SCENARIO Specific set of experimental conditions
+    %   Draws on our database of scenes and assets
+    
+    properties
+        Property1
+    end
+    
+    methods
+        function obj = scenario(varargin)
+            %SCENARIO Construct an instance of this class
 
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
 end
 
