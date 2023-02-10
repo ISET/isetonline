08d4257d25fa51728cd39d719fb17c0bc1abb51f
David Cardinal <github@cardinalphoto.com>
2023-02-09 20:42:25 -0800

    add query sample, attempt to get ap working

--- @idb/docFind.m
+++ @idb/docFind.m
@@ -15,8 +15,17 @@ function documents = docFind(obj,collection, useQuery)
 
    dbTable = 'autoScenesEXR';
    % sceneIDs are unique for auto scenes
+   sceneID = '1112154540';
    queryString = sprintf("{""sceneID"": ""%s""}", sceneID);
    ourScene = ourDB.docFind(dbTable, queryString);
+
+    ourDB = isetdb(); 
+    dbTable = 'sensorImages';
+    filter = 'closestTarget.label';
+    target = 'truck';
+    queryString = sprintf("{""closestTarget.label"": ""%s""}", target);
+    sensorImages = ourDB.docFind(dbTable, queryString);
+    fprintf("Found %d images\n",numel(sensorImages));
 %}
 %
 % D.Cardinal, Stanford University, 2023
--- dataprep/ol_apCompute.m
+++ dataprep/ol_apCompute.m
@@ -8,15 +8,13 @@ function [ap, precision, recall] = ol_apCompute(sensorImages)
 %             Each cell: (.label, .bbox2d, .catId, .distance)
 %         .YOLOData (.bboxes, .scores, .labels) -- arrays of matching size
 
-% I think we have an issue where the YOLOData is scaled to the sensor size,
-% while the GTData is scaled to the scene size. Need to check
-
 %{
 % Test code:
 ourDB = isetdb(); 
 dbTable = 'sensorImages';
-sceneID = '1112154540';
-queryString = sprintf("{""sceneID"": ""%s""}", sceneID);
+filter = 'closestTarget.label';
+target = 'truck';
+queryString = sprintf("{""closestTarget.label"": ""%s""}", target);
 sensorImages = ourDB.docFind(dbTable, queryString);
 
 [ap, precision, recall] = ol_apCompute(sensorImages);
@@ -55,8 +53,12 @@ end
 resultTable = table('Size',[numel(sensorImages) 3],'VariableTypes',{'cell','cell','cell'});
 GTTable = table('Size', [numel(sensorImages) 2], 'VariableTypes',{'cell', 'cell'});
 
+% number of sensor images that have matching classes
+numValid = 0;
+
 for ii = 1:numel(sensorImages)
 
+    fprintf("Processing image %s\n", sensorImages(ii).scenename);
     GTObjects = sensorImages(ii).GTObjects;
     sceneSize = sensorImages(ii).sceneSize;
     detectorResults = sensorImages(ii).YOLOData;
@@ -82,38 +84,59 @@ for ii = 1:numel(sensorImages)
     GTTable(ii,1) = {GTBoxes};
     GTTable(ii,2) = {GTLabels};
 
-
-    tmpBoxes = [];
-    scoreData = [];
     % Now we need to massage our detector results from their DB layout
     % need cells with categoricals, to match Ground Truth
     % HOWEVER, if we have "found" something with a different class
     %          then the call fails, so we need to weed those out. Sigh.
     allLabelData = detectorResults.labels;
     allScoreData = detectorResults.scores;
-    numValid = 0;
 
     % clear out old data
-    tmpBoxes = [];
-    clear labelData;
-    scoreData = [];
-    % We may have an issue where the bboxes from the detector don't match
-    % the scale of the GT image (sigh).
-    for kk = 1:numel(detectorResults.bboxes)
-        % First check to see if valid
-        if max(matches(allLabelData{kk}, GTLabels)) == 0 % non-matched class
-            % do nothing
-        else % okay to process
-            numValid = numValid + 1;
-            tmpBoxes = [tmpBoxes; cell2mat(detectorResults.bboxes{kk})];  %#ok<*AGROW>
-            labelData(numValid) = categorical(cellstr(allLabelData{kk}));
-            scoreData(numValid) = allScoreData{kk};
+    tmpBoxes = {};
+    labelData = [];
+    scoreData = {};
+
+    % Assume not valid as we might have no boxes
+    imgValid = false;
+    if ~isequal(class(allLabelData),'cell')
+        allLabelData = {allLabelData}; % make into a cell
+    end
+        scoreData{numValid+1} = {};
+        ourScoreData = {};
+        ourLabelData = [];
+        for kk = 1:numel(allLabelData)
+
+            if max(matches(allLabelData{kk}, GTLabels)) == 0 % non-matched class
+                % do nothing
+
+            else % okay to process
+                try
+                    tmpBoxes = [tmpBoxes; cell2mat(detectorResults.bboxes{kk})];
+                    % We want score & label to be cell arrays, like boxes
+                    ourScoreData = [ourScoreData allScoreData{kk}]; %#ok<*AGROW> 
+                    ourLabelData = [ourLabelData; cellstr(allLabelData{kk})];
+
+                    numValid = numValid + 1;
+                    imgValid = true;
+                catch
+                    fprintf("failed processing image %s\n", sensorImages(ii).scenename);
+                    imgValid = false;
+                    continue;
+                end
+            end
+        end
+    if imgValid
+        try
+            scoreData = ourScoreData(numValid);
+            labelData = {ourLabelData(numValid)};
+            resultTable(ii,1) = {tmpBoxes};
+            resultTable(ii,2) = {transpose(scoreData)};
+            tmpLabel = transpose(labelData);
+            resultTable(ii,3) = tmpLabel;
+        catch
+            % pause
         end
     end
-
-    resultTable(ii,1) = {tmpBoxes};
-    resultTable(ii,2) = {transpose(scoreData)};
-    resultTable(ii,3) = {transpose(labelData)};
 end
 
 % Buils a box data store now that we have all the GT needed
