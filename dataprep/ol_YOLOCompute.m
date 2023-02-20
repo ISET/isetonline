d5a3e7d7a157c8bc5cd2a024a78f32a77f3cf793
David Cardinal <github@cardinalphoto.com>
2023-02-20 12:40:07 -0800

    fix batch YOLO

--- dataprep/ol_YOLOCompute.m
+++ dataprep/ol_YOLOCompute.m
@@ -8,16 +8,23 @@ if isempty(ourDetector)
     ourDetector = yolov4ObjectDetector(detectorType);
 end
 
-[bboxes, scores, labels] = detect(ourDetector, inputImages);
+[bboxes, scores, labels] = detect(ourDetector, cell2mat(inputImages));
 
 YOLO_Objects.bboxes = bboxes;
 YOLO_Objects.scores = scores;
 YOLO_Objects.labels = labels;
 
+annotatedImages = {};
 % now build annotated image to return
-for ii = 1:numel(YOLO_Objects.bboxes)
-    annotatedImages(ii) = insertObjectAnnotation(img,'Rectangle', ...
-        YOLO_Objects.bboxes(ii), YOLO_Objects.labels(ii));
+for ii = 1:numel(YOLO_Objects)
+    try
+    annotatedImage = insertObjectAnnotation(inputImages{ii},'Rectangle', ...
+        YOLO_Objects(ii).bboxes, YOLO_Objects(ii).labels);
+    annotatedImages{ii} = annotatedImage;
+    catch
+        fprintf('No YOLO data found to annotate');
+        annotatedImages{ii} = inputImages{ii};
+    end
 end
 
 end
