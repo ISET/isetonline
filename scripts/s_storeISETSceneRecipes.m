75b30ec2d83f1d529036b714d4381f8dedc41e18
David Cardinal <github@cardinalphoto.com>
2024-01-16 10:29:33 -0800

    get paths right

--- scripts/s_storeISETSceneRecipes.m
+++ scripts/s_storeISETSceneRecipes.m
@@ -6,22 +6,25 @@
 projectName = 'ISET3d'; % we currently use folders per project
 
 %% Assume our scenes are in folders under
-% /data/scenes, and have the same name as their pbrt file
+% /acorn/data/iset/iset3d-repo/data/scenes, and have the same name as their pbrt file
 
 % Enumerate scenes:
-sceneParentFolder = piDirGet('scenes');
+sceneParentFolder = '/acorn/data/iset/iset3d-repo/data/scenes';
 sceneFolders = dir(sceneParentFolder);
 
+sceneRecipeFiles = {};
 for ii=1:numel(sceneFolders)
-    if isfolder(sceneFolders(ii)) && sceneFolders(ii).name(1)~='.'
+    if sceneFolders(ii).isdir && sceneFolders(ii).name(1)~='.'
         % we have what we hope is a scene folder
-        scenePath = fullfile(sceneFolders(ii).folder, sceneFolders(ii.name));
+        scenePath = fullfile(sceneFolders(ii).folder, sceneFolders(ii).name);
 
         % so check for a pbrt file with the same name
-        if exists(xxx,'file')
+        scenePBRTFile = fullfile(scenePath,[sceneFolders(ii).name '.pbrt']);
+        if exist(scenePBRTFile,'file')
             % now we have one to import
+            sceneRecipeFiles{end+1} = scenePBRTFile; %#ok<SAGROW>
         else
-            fprintf("Mal-formed scene %s found\n", xxx);
+            fprintf("Mal-formed scene %s found\n", sceneFolders(ii).name);
             % skipping
         end
     else
@@ -45,40 +48,23 @@ end
 for ii = 1:numel(sceneRecipeFiles)
 
     % clear these
-    ourRecipe.fileName = '';
-    if isfield(ourRecipe, '_id')
-        ourRecipe = rmfield(ourRecipe,'_id');
-    end
-    p = sceneRecipeFiles(ii).folder;
-    ne = sceneRecipeFiles(ii).name;
-    [~, n, e] = fileparts(sceneRecipeFiles(ii).name);
+    ourRecipe.fileName = sceneRecipeFiles{ii};
+
+    % get the scene id if needed
+    p = sceneRecipeFiles{ii}.folder;
+    ne = sceneRecipeFiles{ii}.name;
+    [~, n, e] = fileparts(sceneRecipeFiles{ii}.name);
 
     % Project-specific metadata
     ourRecipe.project = "ISET3d";
     ourRecipe.creator = "Various";
     ourRecipe.sceneSource = "ISET3d Repo";
     ourRecipe.sceneID = n;
-    ourRecipe.recipeFile = fullfile(sceneRecipeFiles(ii).folder, ...
-        sceneRecipeFiles(ii).name);
+    ourRecipe.recipeFile = sceneRecipeFiles{ii};
 
     % First store the original @recipe info
     ourDB.store(ourRecipe, 'collection', recipeCollection);
 
-    for jj = 1:numel(sceneSuffixes)
-
-        recipeFile = fullfile(p,[n '_' sceneSuffixes{jj} '.pbrt']);
-        if isfile(recipeFile)
-
-            if isfield(ourRecipe, '_id')
-                ourRecipe = rmfield(ourRecipe,'_id');
-            end
-            % Scene specific metadata
-            ourRecipe.lightingType = sceneSuffixes{jj};
-            ourRecipe.fileName = recipeFile;
-
-            ourDB.store(ourRecipe, 'collection', pbrtCollection);
-        end
-    end
 end
 
 
